bf72d9dc96a6be4a893062b623a98c05109cc1b3
David Yun <drumcap@gmail.com>
2021-11-04 20:37:44 +0900

    initial commit

--- runImage.sh
+++ runImage.sh
@@ -1,2 +1,1 @@
-mkdir -p ./data/serverless-docker
-docker run -d --name serverless-docker -p 8080:8080 -p 50000:50000 -v ./data/jenkins-flutter-android:/var/jenkins_home vanillabrain/jenkins-flutter-android:1.0 .
+docker run --rm --name serverless-docker -v ./:/app vanillabrain/serverless-docker:1.0 .
