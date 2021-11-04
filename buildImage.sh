9db78a5665c6b91a2baf32942f92c94890665fb1
dosunyun <vb!@#123>
2021-11-04 20:53:19 +0900

    update buildImage.sh

--- buildImage.sh
+++ buildImage.sh
@@ -1,1 +1,2 @@
 docker build -t vanillabrain/serverless-docker:$(cat VERSION) --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg VERSION=`cat VERSION` .
+docker image tag vanillabrain/serverless-docker:$(cat VERSION) vanillabrain/serverless-docker:latest
